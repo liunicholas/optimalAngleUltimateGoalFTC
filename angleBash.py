a941b265eeef25d670247dbc9bd2e05275fa4d8e
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-10-10 18:34:06 -0700

    no incremental testing all in one try v1
    definitely doesn't work, initial commit

---
+++ angleBash.py
@@ -0,0 +1,103 @@
+#python 3.8.1
+from matplotlib import pyplot
+from numpy import *
+from multiprocessing import Pool
+from sympy.solvers import solveset
+from sympy import Symbol
+#ALL UNITS ARE INCHES AND SECONDS AND DEGREES
+#shooter height
+shooterY = 13.5
+#legal height and distance
+legalY = 5.0*12 + shooterY
+legalX = 11.0*12
+#shooter max velocity
+shooterMaxV = 345.575
+#shooting location
+shootDistMax = 11
+shootDistMin = 6
+#measurements of goal
+goalMax = 38.625
+goalMin = 33.125
+#angles to test
+angleMax = 90
+angleMin = 0
+#increase this value for more precise calculations but longer run time
+p = 1.0
+
+def getAngles(precision):
+    testAngles = []
+    for angle in range(angleMin,angleMax,5/p):
+        testAngles.append(angle)
+
+    return testAngles
+
+def getDistances(precision):
+    testDistances = []
+    for distance in range(shootDistMin, shootDistMax, 1/p):
+        testDistances.append(distance)
+
+    return testDistances
+
+def getVelocity(angle, distance):
+    #two possible velocities from height
+    v = Symbol('v')
+    vVal1 = solveset(Eq((goalMax-shooterY),(math.sin(angle*math.pi/360.0)*v*(distance/(math.cos(angle*math.pi/360.0))))-(0.5*9.8*(distance/(math.cos(angle*math.pi/360.0)))**2)), v)
+    vVal2 = solveset(Eq((goalMin-shooterY),(math.sin(angle*math.pi/360.0)*v*(distance/(math.cos(angle*math.pi/360.0))))-(0.5*9.8*(distance/(math.cos(angle*math.pi/360.0)))**2)), v)
+
+    return vVal1, vVal2
+
+def checkValidV(v, angle):
+    if v <= shooterMaxV:
+        timeMaxHeight = v*sin(angle*math.pi/360.0)/9.8
+        maxHeight = (math.sin(angle*math.pi/360.0)*v*timeMaxHeight)-(0.5*9.8*timeMaxHeight**2)
+        if maxHeight <= legalY:
+            maxDistance = timeMaxHeight*(math.cos(angle*math.pi/360.0))*v
+            if maxDistance <= legalX:
+                return True
+
+    return False
+
+def getPercentHit(vars):
+    angle = vars[0]
+    distances = vars[1]
+
+    in = 0.0
+    for i in range(len(distances)):
+        v1,v2 = getVelocity(angle, distances[i])
+        IN1 = checkValidV(v1, angle)
+        IN2 = checkValidV(v2, angle)
+        if IN1 or IN2:
+            in += 1
+
+    percentIn = in/i*100
+    return percentIn
+
+def displayResults(testAngles, percentInResults):
+    fig = pyplot.figure("Percent In vs Angle")
+    fig.tight_layout()
+    plt = fig.add_subplot(111)
+    plt.set_facecolor('b')
+    plt.scatter(testAngles, percentInResults, c='yellow', marker='s', alpha=0.5)
+    pyplot.subplots_adjust(hspace = 0.4)
+    pyplot.show()
+
+    return
+
+def main():
+    testAngles = getAngles(p)
+    testDistances = getDistances(p)
+
+    vars = []
+    for x in range(len(testAngles)):
+        vars.append([testAngles[x],testDistances)
+
+    #CHANGE PROCESSES TO HOWEVER MANY CORES U HAVE
+    with Pool(processes=4, maxtasksperchild = 1) as pool:
+            percentInResults = pool.map(getPercentHit, vars)
+            pool.close()
+            pool.join()
+
+    displayResults(testAngles, percentInResults)
+
+if __name__ == '__main__':
+	main()
