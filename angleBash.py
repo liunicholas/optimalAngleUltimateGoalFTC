672b9bb96eedc0d4d42bcf66330a5c2e476ee99d
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-10-10 22:53:29 -0700

    pog it works

--- angleBash.py
+++ angleBash.py
@@ -3,13 +3,12 @@ from matplotlib import pyplot
 from numpy import *
 from multiprocessing import Pool
 from sympy import *
-from math import *
 #ALL UNITS ARE INCHES AND SECONDS AND DEGREES
 #shooter height
 shooterY = 13.5
 #legal height and distance
 legalY = 5.0*12 + shooterY
-legalX = 11.0*12
+legalX = 16.0*12
 #shooter max velocity
 shooterMaxV = 345.575
 #shooting location
@@ -19,8 +18,8 @@ shootDistMin = 6*12
 goalMax = 38.625
 goalMin = 33.125
 #angles to test
-angleMax = 60
-angleMin = 20
+angleMax = 80
+angleMin = 0
 #increase this value for more precise calculations but longer run time
 p = 3.0
 
@@ -39,73 +38,90 @@ def getDistances(precision):
     return testDistances
 
 def getVelocity(angle, distance):
+    # print(distance)
     #two possible velocities from height
     v = Symbol('v')
-    # t = (distance/(math.cos(math.radians(angle))*v))
-    vVal1 = solveset(Eq((goalMax-shooterY),(math.sin(math.radians(angle))*v*(distance/(math.cos(math.radians(angle))*v)))-(0.5*32.1522*((distance/(math.cos(math.radians(angle))*v))**2))), v)
-    vVal2 = solveset(Eq((goalMin-shooterY),(math.sin(math.radians(angle))*v*(distance/(math.cos(math.radians(angle))*v)))-(0.5*32.1522*((distance/(math.cos(math.radians(angle))*v))**2))), v)
+    # t = (distance/(cos(radians(angle))*v))
+    vVal1 = solveset(Eq((goalMax-shooterY),(sin(radians(angle))*v*(distance/(cos(radians(angle))*v)))-(0.5*32.1522*((distance/(cos(radians(angle))*v))**2))), v)
+    vVal2 = solveset(Eq((goalMin-shooterY),(sin(radians(angle))*v*(distance/(cos(radians(angle))*v)))-(0.5*32.1522*((distance/(cos(radians(angle))*v))**2))), v)
 
     # print(vVal1,vVal2)
-    return vVal1, vVal2
+    v1, v2 = list(vVal1), list(vVal2)
+    print(v1,v2)
+    # v1 = [x for x in v1 if isinstance(x, float)]
+    # v2 = [x for x in v2 if isinstance(x, float)]
+    # print(v1,v2)
+    return v1, v2
 
-def checkValidV(v, angle):
-    if not v:
-        return False
+def checkValidV(v, angle, distance):
+    print(f"valid velocity: {v}")
+    print(f"angle: {angle}")
+    print(f"distance: {distance}")
     if v <= shooterMaxV:
-        # print(f"valid velocity: {v}")
-        # print(f"angle: {angle}")
-        timeMaxHeight = v*math.sin(math.radians(angle))/32.1522
+        timeMaxHeight = v*sin(radians(angle))/32.1522
         # print(timeMaxHeight)
-        maxHeight = (math.sin(math.radians(angle))*v*timeMaxHeight)-(0.5*32.1522*timeMaxHeight**2)
-        # print(maxHeight)
+        maxHeight = (sin(radians(angle))*v*timeMaxHeight)-(0.5*32.1522*timeMaxHeight**2)
+        print("max height ", maxHeight)
         if maxHeight <= legalY:
             # print("valid height")
-            maxDistance = timeMaxHeight*(math.cos(math.radians(angle)))*v
-            # print(maxDistance)
+            t = Symbol('t')
+            time = solveset(Eq((-1*shooterY),(sin(radians(angle))*v*t)-(0.5*32.1522*(t**2))), t)
+            time = list(time)
+            maxDistance = abs(time[1])*(cos(radians(angle)))*v
+            print("max distance ", maxDistance)
             if maxDistance <= legalX:
-                # print("valid distance")
+                print("valid distance")
                 return True
 
     return False
 
 def getPercentHit(vars):
-    print("trying")
+    # print("trying")
     angle = vars[0]
     distances = vars[1]
 
     totalIn = 0.0
     for i in range(len(distances)):
+        IN1, IN2 = False, False
         v1,v2 = getVelocity(angle, distances[i])
-        if len(v1) != 0:
-            v1=list(v1)
-            for item in v1:
-                try:
-                    if item >= 0:
-                        v = item
-                        IN1 = checkValidV(v, angle)
-                except:
-                    v = None
-                    print('Complex number encountered, moving on...')
-                # if type(item) is not complex:
-                    # if item >= 0:
-                        # v = item
-        if len(v2) != 0:
-            v2=list(v2)
-            for item in v2:
-                try:
-                    if item >= 0:
-                        v = item
-                        IN2 = checkValidV(v, angle)
-                except:
-                    v = None
-                    print('Complex number encountered, moving on...')
-                # if type(item) is not complex:
-                #     if item >= 0:
-                #         v = item
+        IN1 = checkValidV(v1[1], angle, distances[i])
+        IN2 = checkValidV(v2[1], angle, distances[i])
+        # if len(v1) != 0:
+        #     for item in v1:
+        #         if item >= 0:
+        #             v = item
+        #             IN1 = checkValidV(v, angle, distance)
+        #         # try:
+        #         #     if item >= 0:
+        #         #         v = item
+        #         #         IN1 = checkValidV(v, angle, distance)
+        #         # except:
+        #         #     v = 10000
+        #             # print('Complex number encountered, moving on...')
+        #         # if type(item) is not complex:
+        #             # if item >= 0:
+        #                 # v = item
+        # if len(v2) != 0:
+        #     for item in v2:
+        #         if item >= 0:
+        #             v = item
+        #             IN2 = checkValidV(v, angle, distance)
+        #         # try:
+        #         #     if item >= 0:
+        #         #         v = item
+        #         #         IN2 = checkValidV(v, angle, distance)
+        #         # except:
+        #         #     v = 10000
+        #             # print('Complex number encountered, moving on...')
+        #         # if type(item) is not complex:
+        #         #     if item >= 0:
+        #         #         v = item
         if IN1 or IN2:
-            totalIn += 1
+            totalIn += 1.0
 
-    percentIn = totalIn/i*100
+    percentIn = totalIn/(len(distances))
+    print(totalIn)
+    print(len(distances))
     return percentIn
 
 def displayResults(testAngles, percentInResults):
@@ -127,7 +143,7 @@ def main():
     for x in range(len(testAngles)):
         vars.append([testAngles[x],testDistances])
 
-    print(vars)
+    # print(vars)
     #CHANGE PROCESSES TO HOWEVER MANY CORES U HAVE
     with Pool(processes=4, maxtasksperchild = 1) as pool:
             percentInResults = pool.map(getPercentHit, vars)
